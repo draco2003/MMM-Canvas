d1e508157e154ca27eb6a6a1260247931ff53d97
draco2003 <draco2003@gmail.com>
2024-02-18 18:46:21 -0500

    toy with condensing function

--- node_helper.js
+++ node_helper.js
@@ -21,53 +21,22 @@ module.exports = NodeHelper.create({
         var key = payload[0];
         var coursesOld = payload[1];
         var urlbase = payload[2];
-        var count = 0;
         var self = this;
-        var courses = [];
 
-        getCourses();
-        var timer = setInterval(function() {
-            if (count == courses.length) {
-                self.sendSocketNotification('CANVAS_RESULT', finalpayload);
-                finalpayload = [
-                    ["", ""],
-                ];
-                smallpayload = [
-                    ["", ""],
-                ];
-                count = 0;
+        var url = "https://"+ urlbase +"/api/v1/courses?access_token=" + key + "&per_page=30&enrollment_state=active";
+        fetch(url).then(response => response.json()) 
+        .then(courseResult => {
+            for (const course of courseResult) {
+                var course_url = "https://"+ urlbase +"/api/v1/courses/" + course.id + "/assignments?access_token=" + key + "&per_page=30&bucket=upcoming&order_by=due_at";
+                fetch(course_url).then(response => response.json()) 
+                .then(assignmentResult => {
+                    for (var assignment of assignmentResult) {
+                        smallpayload.push([assignment.name, assignment.due_at, course.id]);
+                    }
+                    finalpayload.push(smallpayload);
+                });
             }
-        }, 400);
-
-        function getCourses() {
-            var url = "https://"+ urlbase +"/api/v1/courses?access_token=" + key + "&per_page=30&enrollment_state=active";
-            fetch(url, {
-                method: "GET"
-            })
-            .then(response => console.log(response.status) || response) // output the status and return response
-            .then(response => response.json()) 
-            .then(result => {
-                for (var j in result) {
-                    runCourses(j.id);
-                }
-            });
-        }
-
-        function runCourses(courseId) {
-            var url = "https://"+ urlbase +"/api/v1/courses/" + courseId + "/assignments?access_token=" + key + "&per_page=30&bucket=upcoming&order_by=due_at";
-            fetch(url, {
-                method: "GET"
-            })
-            .then(response => console.log(response.status) || response) // output the status and return response
-            .then(response => response.json()) 
-            .then(result => {
-                for (var j in result) {
-                    smallpayload.push([result[j].name, result[j].due_at, index]);
-                }
-                finalpayload.push(smallpayload);
-                count++;
-            });
-        }
+        }).then(self.sendSocketNotification('CANVAS_RESULT', finalpayload));
     },
 
     socketNotificationReceived: function(notification, payload) {
